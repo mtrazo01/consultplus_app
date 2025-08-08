6de3d98dd2535a22c82be0c3259db4177a56411b
mtrazo01 <mtrazo01@gmail.com>
2025-08-07 23:16:19 -0300

    Backend pronto

--- consultplus-novo/src/services/api.js
+++ consultplus-novo/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.15.185:3000', 
+  baseURL: 'https://consultplus-backend.onrender.com',
 });
-
+ 
 export default api;
